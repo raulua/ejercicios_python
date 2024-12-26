f07a53141a6b89655b95efee645595df4f6d5dfa
Raul Utrera Agraz <144369660+raulua@users.noreply.github.com>
2024-12-26 12:33:59 +0100

    Create suma_primos_funcion.py
    Suma de los veinte primeros números primos, usando funciones

---
+++ suma_primos_funcion.py
@@ -0,0 +1,19 @@
+def es_primo(div):
+    for n in range(2, div):
+        if num % n == 0:
+#            print("No es número primo", n, "es divisor")
+            return False
+    print(div, "es número primo")
+    return True
+
+num = 2
+suma = 0
+cont = 0
+
+while cont < 20:
+    if es_primo(num):
+        suma += num
+        cont += 1
+    num +=1
+
+print ("La suma de los veinte primeros números primos es", suma)
